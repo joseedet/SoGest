fdd24bff2ad4683118def47c5aaf92ae6838eaa8
Morfeo68 <joseedet@gmail.com>
2021-10-27 10:25:05 +0200

    HomeController,ErrorViewModel,Index-Home,Layout,wwwroot->site,wwwroot ->bootstrap

--- SoGest/SoGest.UI/Controllers/HomeController.cs
+++ SoGest/SoGest.UI/Controllers/HomeController.cs
@@ -1,13 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
+using AplicacionComercial.Web.Models;
+
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+using SoGest.Data.Interaces;
+using SoGest.Data.Repositories.Context;
 
 namespace SoGest.UI.Controllers
 {
     public class HomeController:Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController ( ILogger<HomeController> logger )
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index ()
+        {
+            return View( );
+        }
+
+        public IActionResult Privacy ()
+        {
+            return View( );
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error ()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
